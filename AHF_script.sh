55680c792d959844674ab3406cc7e1e5a8bdeac8
ZenenHornstein <49211357+ZenenHornstein@users.noreply.github.com>
2019-07-11 10:53:45 -0700

    Update AHF_script.sh

--- AHF_script.sh
+++ AHF_script.sh
@@ -1,5 +1,3 @@
-  GNU nano 2.7.4                 File: AHF_script.sh                            
-
 #!/bin/bash
 # This script should completely set up AutoHeadFixer on a blank Raspberry pi.
 #$ sudo chmod 700 AFH_script.sh
@@ -22,21 +20,51 @@ cd AutoHeadFixSetup
 #sudo apt-get upgrade -y
 
 echo "downloading AHF repository lever_config branch"
-git clone --single-branch --branch lever_config https://github.com/jamieboyd/Au$
-
+git clone --single-branch --branch lever_config https://github.com/jamieboyd/AutoHeadFix/
 
 echo "cloning pulsedThread and building makefile"
 git clone https://github.com/jamieboyd/pulsedThread.git
 cd pulsedThread
 sudo make
 sudo make install
+mv * ..
 cd ..
+rm pulsedThread
 
 
 echo "Cloning GPIO_Thread"
 git clone https://github.com/jamieboyd/GPIO_Thread.git
+cd GPIO_Thread
+mv * ..
+rm GPIO_Thread
 
- 
+echo "cloning rfid reader"
+git clone https://github.com/jamieboyd/RFIDTagReader.git
+cd RFIDTagReader
+mv * ..
+rm RFIDTagReader
+
+echo "cloning adafruit python mpr121"
+git clone https://github.com/adafruit/Adafruit_Python_MPR121.git
+cd Adafruit_Python_MPR121
+mv * ..
+rm Adafruit_Python_MPR121
+
+echo "cloning adafruit GPIO"
+git clone https://github.com/adafruit/Adafruit_Python_GPIO.git 
+cd Adafruit_Python_GPIO
+mv * ..
+rm Adafruit_Python_GPIO
+
+
+echo "cloning touch detector"
+git clone https://github.com/jamieboyd/TouchDetector.git
+cd TouchDetector
+mv * ..
+rm TouchDetector
+
+
+echo "installing pypy and remaining modules (mysql-server, php-mysql, pymysql)"
 sudo apt-get install pypy mysql-server php-mysql -y
 python3 -m pip install PyMySQL
 pip3 install adafruit-circuitpython-mpr121
@@ -51,4 +79,6 @@ if [ $PURGE = true ];
 fi
 
 echo "Install complete"
+
+
 

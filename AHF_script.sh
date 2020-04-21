ef6527c45a651baf5a858e179b160dda3128f19f
Alice-Xiong <alicexxx0516@gmail.com>
2020-04-20 21:16:29 -0700

    clone main branch

--- AHF_script.sh
+++ AHF_script.sh
@@ -138,7 +138,7 @@ if [ $userinput -eq 0 ];
 		python3 -m pip install PyMySQL 
 													 
 		echo "downloading AHF repository closed-loop branch"
-		git clone https://github.com/ubcbraincircuits/AutoHeadFix -b closed-loop
+		git clone https://github.com/ubcbraincircuits/AutoHeadFix
 		cd AutoHeadFix
 		_path=$PWD
 		echo $_path
